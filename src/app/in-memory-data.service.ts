4ecc02327db312b817321aea60b484aa65dbe124
Adam Azira <aladdintirta@gmail.com>
2021-11-23 19:18:52 +0700

    add some other heroes

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -18,6 +18,13 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 18, name: 'Mohammad Hatta'},
       { id: 19, name: 'Abdul Kadir Raden Temenggung'},
       { id: 20, name: 'Thomas Matulessy'},
+      { id: 21, name: 'Raden Dewi Sartika'},
+      { id: 22, name: 'Teuku Umar'},
+      { id: 23, name: 'Tuanku Imam Bondjol'},
+      { id: 25, name: 'Sudirman'},
+      { id: 26, name: 'Pangeran Antasari'},
+      { id: 27, name: 'Mohammad Natsir'},
+      { id: 28, name: 'Gatot Subroto'},
     ]
     return { heroes };
   }
